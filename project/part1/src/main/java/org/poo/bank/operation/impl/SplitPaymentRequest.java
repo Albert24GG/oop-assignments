1daaebadba58c5de570c0686a818e11c08e3f3e0
Albert24GG <guiman.albert09@gmail.com>
2024-12-14 18:58:07 +0200

    fix: Fix split payment log

--- project/part1/src/main/java/org/poo/bank/operation/impl/SplitPaymentRequest.java
+++ project/part1/src/main/java/org/poo/bank/operation/impl/SplitPaymentRequest.java
@@ -43,7 +43,8 @@ public final class SplitPaymentRequest extends BankOperation<Void> {
                 .toList();
 
         double splitAmount = amount / accounts.size();
-        List<Double> amounts = accounts.stream()
+        // Convert the split amount to the currency of each account
+        List<Double> convertedSplitAmounts = accounts.stream()
                 .map(account -> context.currencyExchangeService()
                         .convert(currency, account.getCurrency(), splitAmount))
                 .toList();
@@ -51,7 +52,7 @@ public final class SplitPaymentRequest extends BankOperation<Void> {
         // Check if all accounts have enough funds
         Optional<BankAccount> lastAccountWithInsufficientFunds = IntStream.range(0, accounts.size())
                 .filter(
-                        i -> accounts.get(i).getBalance() < amounts.get(i)
+                        i -> accounts.get(i).getBalance() < convertedSplitAmounts.get(i)
                 )
                 .reduce((a, b) -> b).stream()
                 .mapToObj(accounts::get)
@@ -59,29 +60,31 @@ public final class SplitPaymentRequest extends BankOperation<Void> {
 
         // Prepare the transaction log
         var logBuilder = SplitPaymentLog.builder()
+                .description(
+                        String.format("Split payment of %.2f %s", amount, currency))
                 .timestamp(timestamp)
                 .amount(splitAmount)
                 .involvedAccounts(involvedAccounts)
                 .currency(currency);
 
 
-        TransactionLog transactionLog = lastAccountWithInsufficientFunds.<TransactionLog>map(
+        // If at least one account has insufficient funds, log the error and return
+        SplitPaymentLog transactionLog = lastAccountWithInsufficientFunds.<SplitPaymentLog>map(
                 account -> logBuilder
                         .error(String.format(
-                                "Account %s has insufficient funds for a split payment",
+                                "Account %s has insufficient funds for a split payment.",
                                 account.getIban()))
                         .build()
         ).orElseGet(
                 () -> {
                     IntStream.range(0, accounts.size())
                             .forEach(i -> context.bankAccService()
-                                    .removeFunds(accounts.get(i), amounts.get(i)));
-                    return logBuilder
-                            .description(
-                                    String.format("Split payment of %.2f %s", amount, currency))
-                            .build();
+                                    .removeFunds(accounts.get(i), convertedSplitAmounts.get(i)));
+                    return logBuilder.build();
                 }
+
         );
+
         accounts.forEach(account -> context.transactionService()
                 .logTransaction(account.getIban(), transactionLog));
         return BankOperationResult.success();
