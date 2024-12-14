7337ad60ced13fa0ba0e113efe1057cc70484c64
Albert24GG <guiman.albert09@gmail.com>
2024-12-14 20:49:09 +0200

    fix: Remove email address validation

--- project/part1/src/main/java/org/poo/bank/type/Email.java
+++ project/part1/src/main/java/org/poo/bank/type/Email.java
@@ -1,28 +1,9 @@
 package org.poo.bank.type;
 
 import com.fasterxml.jackson.annotation.JsonValue;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NonNull;
 
-@Getter
-@EqualsAndHashCode
-public final class Email {
-    private final String value;
-
-    /**
-     * Constructs an Email object.
-     *
-     * @param value the email address
-     * @throws IllegalArgumentException if the email address is invalid
-     */
-    public Email(@NonNull final String value) {
-        if (!value.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")) {
-            throw new IllegalArgumentException("Invalid email address");
-        }
-        this.value = value;
-    }
-
+public record Email(String value) {
     /**
      * Creates an Email object.
      *
