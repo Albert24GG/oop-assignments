28e06be8d1e8eb9fc5c05fa37a7a3928341399c4
Albert24GG <guiman.albert09@gmail.com>
2025-01-03 15:03:35 +0200

    feat: Add new type for representing dates

--- project/part2/src/main/java/org/poo/bank/type/Date.java
+++ project/part2/src/main/java/org/poo/bank/type/Date.java
@@ -1,4 +1,30 @@
 package org.poo.bank.type;
 
-public class Date {
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+
+public record Date(@NonNull LocalDate date) {
+    /**
+     * Creates a Date object.
+     *
+     * @param date the date string, in the format "yyyy-MM-dd"
+     * @return the Date object
+     */
+    public static Date of(@NonNull final String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        return new Date(LocalDate.parse(date, formatter));
+    }
+
+    /**
+     * Calculates the period between this date and the end date as a Period.
+     * @param endDateExclusive the end date
+     * @return the period between this date and the end date
+     */
+    public Period until(final ChronoLocalDate endDateExclusive) {
+        return date.until(endDateExclusive);
+    }
 }
