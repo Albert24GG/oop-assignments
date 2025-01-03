b3e77a34d48dc3e239b09a29f3fe61043c9739bc
Albert24GG <guiman.albert09@gmail.com>
2025-01-03 19:20:22 +0200

    fix: Round account balance view to 2 decimals

--- project/part2/src/main/java/org/poo/bank/account/BankAccView.java
+++ project/part2/src/main/java/org/poo/bank/account/BankAccView.java
@@ -7,6 +7,8 @@ import org.poo.bank.card.CardView;
 import org.poo.bank.type.Currency;
 import org.poo.bank.type.IBAN;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 @Builder(access = lombok.AccessLevel.PRIVATE)
@@ -28,7 +30,8 @@ public final class BankAccView {
     public static BankAccView from(final BankAccount account) {
         return BankAccView.builder()
                 .iban(account.getIban())
-                .balance(account.getBalance())
+                .balance(new BigDecimal(Double.toString(account.getBalance())).setScale(2,
+                        RoundingMode.HALF_UP).doubleValue())
                 .currency(account.getCurrency())
                 .type(account.getType())
                 .cards(account.getCards().stream()
